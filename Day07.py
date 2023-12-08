64b3f49649d009e4ed2fcbfa9ced1b1e109f396a
halberfan <39102685+halberfan@users.noreply.github.com>
2023-12-08 16:27:42 +0100

    part 2 added

--- Day07.py
+++ Day07.py
@@ -29,6 +29,7 @@ for line in lines:
     else: print(s)
 
 def part_one():
+    ranks['J'] = 9
     points = [[],[],[],[],[],[],[],[]]
     
     for hand in hands:
@@ -38,8 +39,9 @@ def part_one():
         points[p].append(hand)
 
     rank = []
+    key_cmp = cmp_to_key(compare_two_hands)
     for p_a in points:
-        for p in sorted(p_a, key=cmp_to_key(compare_two_hands)):
+        for p in sorted(p_a, key=key_cmp):
             rank.insert(0, p)
     r_len = len(rank)
     sum = 0
@@ -49,13 +51,36 @@ def part_one():
         #print(rank[i], hands[rank[i]], i, get_hand_type(rank[i]))
     return sum
 
+def part_two():
+    ranks['J'] = -1
+    points = [[],[],[],[],[],[],[],[]]
+    
+    for hand in hands:
+        p = get_hand_type_considering_joker(hand)
+        if(points[p] == None):
+            points[p] = []
+        points[p].append(hand)
+    
+    rank = []
+    key_cmp = cmp_to_key(compare_two_hands)
+    for p_a in points:
+        for p in sorted(p_a, key=key_cmp):
+            rank.insert(0, p)
+    r_len = len(rank)
+    sum = 0
+    rank.reverse()
+    for i in (range(0, r_len)):
+        sum += (i+1) * hands[rank[i]]
+        #print(rank[i], hands[rank[i]], i, get_hand_type_considering_joker(rank[i]))
+    return sum
+
 def compare_two_hands(hand1:str, hand2:str):
     for c in range(0,5):
         if hand1[c] != hand2[c]:
             if(ranks[hand1[c]] > ranks[hand2[c]]):
                 return 1
             else: return -1
-    return 0
+    #return 0
 
 def get_hand_type(hand: str):
     amount_of_chars = dict(Counter(hand))
@@ -64,28 +89,39 @@ def get_hand_type(hand: str):
     distinct_counter = 0
     for c in amount_of_chars:
         if amount_of_chars[c] == 5:
-            return 7
+            return 7 # 7: Kniffel
         elif amount_of_chars[c] == 4:
-            return 6
+            return 6 # 6: Vierer Pasch
         elif amount_of_chars[c] == 3:
             if pair:
-                return 5
+                return 5 # 5: Full House
             else: three_pair = True
         elif amount_of_chars[c] == 2:
             if pair:
-                return 3
+                return 3 # 3: 2 2-er Paare
             else:
                 pair = True
         elif amount_of_chars[c] == 1:
             distinct_counter += 1
     if pair:
         if three_pair:
-            return 5
-        return 2
+            return 5 # 5: Full House
+        return 2 # 2: Ein Paar
     elif distinct_counter == 5:
-        return 1
+        return 1 # 1: Straße
     if three_pair:
-        return 4
-    return 0
+        return 4 # 4: Dreier Pasch
+    return 0 # 0: Nichts davon
+
+def get_hand_type_considering_joker(hand: str):
+    highest_yet = get_hand_type(hand)
+    jokers = hand.count('J')
+    for i in range(jokers):
+        for r in ranks:
+            type = get_hand_type(hand.replace('J', r, i + 1))
+            if type > highest_yet:
+                highest_yet = type
+    return highest_yet
 
 print(part_one())
+print(part_two())
